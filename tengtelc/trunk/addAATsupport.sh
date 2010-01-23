2c6869d37dfbf3e6cc54e84fbbd2f808fabe3763
j-mach-wust <j-mach-wust@07180bc1-4d05-40e4-83f0-00087d37d8a0>
2010-01-23 16:39:04 +0000

    a script for changing tengtelc glyph names

---
+++ tengtelc/trunk/addAATsupport.sh
@@ -0,0 +1,8 @@
+CpMac tengtelc.ttf tengtelc-ftx.ttf
+CpMac tengtelcb.ttf tengtelcb-ftx.ttf
+ftxdumperfuser -t post -o tengtelc-ftx.post.xml tengtelc-ftx.ttf 
+ftxdumperfuser -t post -o tengtelcb-ftx.post.xml tengtelcb-ftx.ttf 
+./addtengwarglyphnames.sh tengtelc-ftx.post.xml 
+./addtengwarglyphnames.sh tengtelcb-ftx.post.xml 
+ftxdumperfuser -t post -d tengtelc-ftx.post.xml tengtelc-ftx.ttf 
+ftxdumperfuser -t post -d tengtelcb-ftx.post.xml tengtelcb-ftx.ttf 
