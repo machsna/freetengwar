3869806bd599122675027f6fcdc77ffb6df269e1
Johan Winge <johan.winge@gmail.com>
2010-01-25 19:33:39 +0000

    Changed to descriptive glyph names.

--- tengtelc/trunk/addAATsupport.sh
+++ tengtelc/trunk/addAATsupport.sh
@@ -2,11 +2,6 @@
 
 CpMac tengtelc.ttf tengtelc-ftx.ttf
 CpMac tengtelcb.ttf tengtelcb-ftx.ttf
-ftxdumperfuser -t post -o tengtelc-ftx.post.xml tengtelc-ftx.ttf 
-ftxdumperfuser -t post -o tengtelcb-ftx.post.xml tengtelcb-ftx.ttf 
-./addtengwarglyphnames.sh tengtelc-ftx.post.xml 
-./addtengwarglyphnames.sh tengtelcb-ftx.post.xml 
-ftxdumperfuser -t post -d tengtelc-ftx.post.xml tengtelc-ftx.ttf 
-ftxdumperfuser -t post -d tengtelcb-ftx.post.xml tengtelcb-ftx.ttf 
 ftxenhancer -m tengtelc-ftx.mif tengtelc-ftx.ttf
 ftxenhancer -m tengtelc-ftx.mif tengtelcb-ftx.ttf
+
