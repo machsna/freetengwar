4d905fbfc5c0169881f997ac9f06fafa06203988
j-mach-wust <j-mach-wust@07180bc1-4d05-40e4-83f0-00087d37d8a0>
2010-01-27 20:00:46 +0000

    AAT support; sarince choice (AAT and Graphite); new characters open anna, vaiya, grave tehta, dot-inside tehta; glyph changes prefeanorian quesse, lowdham HW

--- formal/trunk/compile.sh
+++ formal/trunk/compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-fontforge -script dansmith2unicode.py
-grcompiler TengwarFormalUC.gdl TengwarFormalUC_dumb.ttf TengwarFormalUC.ttf
-# And maybe also "ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC.ttf"?
-
+# fontforge -script dansmith2unicode.py
+grcompiler TengwarFormalUC.gdl TengwarFormalUnicode-Regular.ttf TengwarFormalUC.ttf
+ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC.ttf
