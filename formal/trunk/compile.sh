c7e1728cbbbf0cf21d4dc800c4a17212d43338ce
alatius <alatius@07180bc1-4d05-40e4-83f0-00087d37d8a0>
2010-01-29 16:59:00 +0000

    Incorporate original font files in the workflow again.

--- formal/trunk/compile.sh
+++ formal/trunk/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# fontforge -script dansmith2unicode.py
-grcompiler TengwarFormalUC.gdl TengwarFormalUnicode-Regular.ttf TengwarFormalUC.ttf
+fontforge -script dansmith2unicode.py
+grcompiler TengwarFormalUC.gdl TengwarFormalUC_dumb.ttf TengwarFormalUC.ttf
 ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC.ttf
