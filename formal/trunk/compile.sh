907a3620f5c30401483ae26dbe9855495d67498a
J. ‘mach’ Wust <j_mach_wust@shared-files.de>
2010-01-30 13:39:17 +0000

    the MIF files are much less complicated now that I've understood the DEL class; added a XeLaTeX testcase for the short carrier insertion

--- formal/trunk/compile.sh
+++ formal/trunk/compile.sh
@@ -3,3 +3,6 @@
 fontforge -script dansmith2unicode.py
 grcompiler TengwarFormalUC.gdl TengwarFormalUC_dumb.ttf TengwarFormalUC.ttf
 ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC.ttf
+
+CpMac TengwarFormalUC_dumb.ttf TengwarFormalUC-shortCarrierInsertion_nogr.ttf
+ftxenhancer -m TengwarFormalUC-shortCarrierInsertion.mif TengwarFormalUC-shortCarrierInsertion_nogr.ttf
