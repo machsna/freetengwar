09aa874e26e06efdeab6542bc236cc989534abba
J. ‘mach’ Wust <j_mach_wust@shared-files.de>
2010-02-10 20:52:18 +0000

    TengwarFormalUC_AAT.ttf added so an AAT + Graphite TTF can be compiled on systems other than Mac; obsolete carrier insertion testcase deleted

--- formal/trunk/compile.sh
+++ formal/trunk/compile.sh
@@ -2,5 +2,7 @@
 
 fontforge -script dansmith2unicode.py
 
-ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC_dumb.ttf
-grcompiler TengwarFormalUC.gdl TengwarFormalUC_dumb.ttf TengwarFormalUC.ttf
+cp TengwarFormalUC_dumb.ttf TengwarFormalUC_AAT.ttf
+ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC_AAT.ttf
+
+grcompiler TengwarFormalUC.gdl TengwarFormalUC_AAT.ttf TengwarFormalUC.ttf
