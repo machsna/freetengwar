5c6069c3538bf076019aa986db9ca226603999ed
j-mach-wust <j-mach-wust@07180bc1-4d05-40e4-83f0-00087d37d8a0>
2010-02-10 12:02:26 +0000

    more adjustments; more comparison tables in the PDF; seperate carrier insertion font deleted

--- formal/trunk/compile.sh
+++ formal/trunk/compile.sh
@@ -2,8 +2,5 @@
 
 fontforge -script dansmith2unicode.py
 
-CpMac TengwarFormalUC_dumb.ttf TengwarFormalUC-shortCarrierInsertion_nogr.ttf
-ftxenhancer -m TengwarFormalUC-shortCarrierInsertion.mif TengwarFormalUC-shortCarrierInsertion_nogr.ttf
-
 ftxenhancer -m TengwarFormalUC.mif TengwarFormalUC_dumb.ttf
 grcompiler TengwarFormalUC.gdl TengwarFormalUC_dumb.ttf TengwarFormalUC.ttf
