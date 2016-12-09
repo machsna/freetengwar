21a0d9639f421a4c3869e5f1a0c7cdb8766f7aa1
Johan Winge <johan.winge@gmail.com>
2016-12-09 22:17:43 +0000

    Retire current trunk, going back to r180.

---
+++ tengtelc/branches/shiftedtehtar/addAATsupport.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CpMac tengtelc.ttf tengtelc-incompleteAAT.ttf
+CpMac tengtelcb.ttf tengtelcb-incompleteAAT.ttf
+ftxenhancer -m tengtelc-AAT.mif tengtelc-incompleteAAT.ttf
+ftxenhancer -m tengtelc-AAT.mif tengtelcb-incompleteAAT.ttf
+
